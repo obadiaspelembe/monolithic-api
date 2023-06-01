24ae25e9e24d72f3dfc77f5939278bc8b4ab40df
Obadias Pelembe <52324498+obadiaspelembe@users.noreply.github.com>
2023-06-01 18:17:27 +0200

    refactor: re-structure project modules (#6)

--- modules/core/src/main.py
+++ modules/core/src/main.py
@@ -5,9 +5,9 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 
-@app.get("/m_custom_fields/")
+@app.get("/core/")
 async def root():
-    return HTMLResponse(content="Hello there from custom_fields root", status_code=200)
+    return HTMLResponse(content="Hello there from core root", status_code=200)
 
 @app.get("/pet")
 async def pet():
